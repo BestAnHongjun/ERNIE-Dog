219f2b7e802ce06e21a10e51f50037f2d9f400ed
Coder.AN <an.hongjun@foxmail.com>
2024-02-06 09:36:32 +0800

    更新了反馈机制

--- demo.py
+++ demo.py
@@ -20,16 +20,22 @@ name2func = {
 
 if __name__ == "__main__":
     command = '以初始位置为参考，你左前方30度方向、距离3m有一个红色的小球；右前方30度方向、距离1m有一个蓝色的小球；你正前方2m，有一个绿色的小球。请先走到蓝色的小球前面，叫一声，然后发出灯光。'
+    print(" >>> 用户指令：", command, "\n")
 
     while True:
         ret, action = bot.get_actions(command)
-        print(action)
-        if not ret: break 
-
+        if ret:
+            print(" <<< 文心大模型：", action["thoughts"])
+            print("   - 调用方法：", action["name"])
+            print("   - 调用参数：", action["arguments"])
+        else:
+            print(" <<< 文心大模型：", action)
+            break
+        
         # 本DEMO默认Bot返回结果正确，实际应用时请对参数正确性进行检查，并对异常情况做相应处理。
         func_call = name2func[action['name']]
         func_args = json.loads(action["arguments"])
         func_res = func_call(**func_args)
-        print(" >>> 反馈：", func_res)
+        print(" >>> 反馈：", func_res, "\n")
         
         command = func_res    
